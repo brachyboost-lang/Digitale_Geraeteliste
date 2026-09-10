5edf01e07e6688becd3b8f6ded93292742c82a18
Alexander Lang <Alexander.Lang@it-lang.de>
2026-09-10 19:56:51 +0200

    Employee and Item class Properties added First Seed Samples generated

---
+++ Digitale_Geraeteliste/Core/Model/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Digitale_Geraeteliste.Core.Model
+{
+    internal class Employee
+    {
+        public int Id { get; set; }
+        public string LastName { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Department { get; set; } = string.Empty;
+
+    }
+}
---
+++ Digitale_Geraeteliste/Core/Model/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Digitale_Geraeteliste.Core.Model
+{
+    internal class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Type { get; set; } = string.Empty;
+        public int standardLendDuration { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsRetired { get; set; }
+
+    }
+}
