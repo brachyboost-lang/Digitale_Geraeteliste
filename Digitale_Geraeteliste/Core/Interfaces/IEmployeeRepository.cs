063247547ec64e0e13f169709c0edf3912999ae8
Alexander Lang <Alexander.Lang@it-lang.de>
2026-09-14 07:15:41 +0200

    filled interfaces with abstract methods

--- Digitale_Geraeteliste/Core/Interfaces/IEmployeeRepository.cs
+++ Digitale_Geraeteliste/Core/Interfaces/IEmployeeRepository.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Digitale_Geraeteliste.Core.Model;
 
 namespace Digitale_Geraeteliste.Core.Interfaces
 {
     public interface IEmployeeRepository
     {
+        public abstract Employee CreateNewEmployee(string firstName, string lastName, string email, string phoneNumber, string department);
+        public abstract Employee ChangeEmployee(Employee employee, string firstName, string lastName, string email, string phoneNumber, string department);
+        public abstract Employee SaveEmployee(Employee employee);
+        public abstract Employee GetEmployeeByID(int id);
     }
 }
--- Digitale_Geraeteliste/Core/Interfaces/ILendItemRepository.cs
+++ Digitale_Geraeteliste/Core/Interfaces/ILendItemRepository.cs
@@ -1,10 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.RightsManagement;
 using System.Text;
+using Digitale_Geraeteliste.Core.Model;
 
 namespace Digitale_Geraeteliste.Core.Interfaces
 {
     public interface ILendItemRepository
     {
+        public abstract LendItem CreateNewLendItem(int itemId, int employeeId, DateTime lendDate, DateTime returnDate);
+        public abstract LendItem GetLendItemById(int id);
+        public abstract IEnumerable<LendItem> GetAllLendItems();
+        public abstract void ChangeLendItem(LendItem lendItem, int itemId, int employeeId, DateTime lendDate, DateTime returnDate);
+        public abstract void SaveLendItem(LendItem lendItem);
+        public abstract void ReturnLendItem(int lendItemId, DateTime returnDate);
+
     }
 }
