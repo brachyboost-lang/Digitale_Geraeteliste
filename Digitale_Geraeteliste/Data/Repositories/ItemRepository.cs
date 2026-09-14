6bca25101a9aad022d9f14eddd80e86ef07f85e9
Alexander Lang <Alexander.Lang@it-lang.de>
2026-09-14 09:54:18 +0200

    implemented dbcontext

--- Digitale_Geraeteliste/Data/Repositories/ItemRepository.cs
+++ Digitale_Geraeteliste/Data/Repositories/ItemRepository.cs
@@ -17,25 +17,41 @@ namespace Digitale_Geraeteliste.Data.Repositories
         }
         public Item GetItemById(int id)
         {
-            var allItems = GetAllItems();
-            return allItems.FirstOrDefault(i => i.Id == id);
+            try
+            {
+                var allItems = GetAllItems();
+                Item itemById = allItems.First(i => i.Id == id);
+                return itemById;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("An error occurred while retrieving the item by ID. Check for Typo or Item might not exist.", ex);
+            }
         }
         public IEnumerable<Item> GetAllItems()
         {
-            return Context.Items.Include(i => i.Name).ToList();
+            return Context.Items.Include(i => i.Category).ToList();
         }
         public void ChangeItem(Item item, string inventoryNumber, string name, Category category, string description, int standardLendDuration, bool isInUse, bool isRetired, bool needsMaintenance)
         {
-            item.InventoryNumber = inventoryNumber;
-            item.Name = name;
-            item.Category = category;
-            item.CategoryId = category.Id;
-            item.Description = description;
-            item.StandardLendDuration = standardLendDuration;
-            item.IsInUse = isInUse;
-            item.IsRetired = isRetired;
-            item.NeedsMaintenance = needsMaintenance;
-            SaveItem(item);
+            Item itemToChange = GetItemById(item.Id);
+            itemToChange.InventoryNumber = inventoryNumber;
+            itemToChange.Name = name;
+            itemToChange.Category = category;
+            itemToChange.CategoryId = category.Id;
+            itemToChange.Description = description;
+            itemToChange.StandardLendDuration = standardLendDuration;
+            itemToChange.IsInUse = isInUse;
+            itemToChange.IsRetired = isRetired;
+            itemToChange.NeedsMaintenance = needsMaintenance;
+            try
+            {
+            UpdateItem(itemToChange);
+            }
+            catch (Exception ex) // double it and give it to the next person
+            {
+                throw new Exception("An error occurred while updating the Database. Contact your system administrator.", ex); 
+            }
         }
         public bool CheckInventoryNumberDuplicate(string inventoryNumber)
         {
@@ -43,9 +59,10 @@ namespace Digitale_Geraeteliste.Data.Repositories
             return allItems.Any(i => i.InventoryNumber == inventoryNumber);
         }
 
-        public void SaveItem(Item item)
+        public void UpdateItem(Item item)
         {
-            
+            Context.Items.Update(item);
+            Context.SaveChanges();
         }
     }
 }
