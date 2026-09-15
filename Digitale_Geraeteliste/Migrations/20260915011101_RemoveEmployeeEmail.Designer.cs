728ae2d8aa534aed8e362a948606a2054b037cab
Alexander Lang <Alexander.Lang@it-lang.de>
2026-09-15 03:12:38 +0200

    updated database

--- Digitale_Geraeteliste/Migrations/20260915011101_RemoveEmployeeEmail.Designer.cs
+++ Digitale_Geraeteliste/Migrations/20260915011101_RemoveEmployeeEmail.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Digitale_Geraeteliste.Data.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Digitale_Geraeteliste.Migrations
 {
     [DbContext(typeof(LendContext))]
-    partial class LendContextModelSnapshot : ModelSnapshot
+    [Migration("20260915011101_RemoveEmployeeEmail")]
+    partial class RemoveEmployeeEmail
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.12");
---
+++ Digitale_Geraeteliste/Migrations/20260915011101_RemoveEmployeeEmail.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Digitale_Geraeteliste.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveEmployeeEmail : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Employees");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Employees",
+                type: "TEXT",
+                nullable: true);
+        }
+    }
+}
